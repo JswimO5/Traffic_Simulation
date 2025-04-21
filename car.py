ce3e18a9ec27614f1cfa888b8774a7fefdbb0394
jdurcan05 <112487438+jdurcan05@users.noreply.github.com>
2025-04-21 19:03:08 -0400

    Barebones class that will be improved upon. Utilizes enums

--- car.py
+++ car.py
@@ -8,10 +8,21 @@ class Car:
     MAIN_STREET_NORTH = 10000
     MAIN_STREET_SOUTH = 13500
     CONCORD = 14000
-
+    total = GRIFFITH_TO_HIGHWAY + BEATY + MAIN_STREET_NORTH + MAIN_STREET_SOUTH + CONCORD
 
     roadFrom = None
     roadTo = None
 
     def __init__(self, roadFrom):
         self.roadFrom = roadFrom
+        rand = random.randint(1, self.total)
+        if rand <= self.GRIFFITH_TO_HIGHWAY:
+            self.roadTo = 1
+        elif rand <= self.GRIFFITH_TO_HIGHWAY + self.BEATY:
+            self.roadTo = 2
+        elif rand <= self.GRIFFITH_TO_HIGHWAY + self.BEATY + self.MAIN_STREET_NORTH:
+            self.roadTo = 3
+        elif rand <= self.GRIFFITH_TO_HIGHWAY + self.BEATY + self.MAIN_STREET_NORTH + self.MAIN_STREET_SOUTH:
+            self.roadTo = 4
+        else:
+            self.roadTo = 5
