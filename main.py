53ab62f0196bda76736e12008af79e59283998c2
JswimO5 <jmcdo1swim@gmail.com>
2025-05-05 09:46:45 -0400

    small optimizations to the simulation
    made it so it ran for 15 minutes before taking any data.

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from gameboard_post_construction import GameBoard2
 def main():
     time = 0
     hour = 3600
-    board = GameBoard(hour)
+    board = GameBoard(1.25 * hour)
     sum = 0
     num_cars = 0
     while time < 1.5*hour:
         commutes = board.time_seg(time)
-        if(len(commutes)>0):
-            for i in range(len(commutes)):
-                sum += time - commutes[i].get_spawn_time()
-                num_cars += 1
-        if time%500 == 0:
+        if time > 0.25 * hour:
+            if(len(commutes)>0):
+                for i in range(len(commutes)):
+                    sum += time - commutes[i].get_spawn_time()
+                    num_cars += 1
+        if time%599 == 0:
             board.print_all()
         time += 1
     average_commute = sum/num_cars
-    print(f"The average commute time is {average_commute/2}s\n")
+    print(f"The average commute time is {(average_commute/60):.2f} min\n")
     print(num_cars)
 
 main()
